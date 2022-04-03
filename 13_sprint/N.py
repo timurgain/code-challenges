289118469e4e7f68da399467cadd2e2bb200491f
Timur Gain <timur.gain@gmail.com>
2022-04-03 23:30:51 +0300

    one error left

--- 13_sprint/N.py
+++ 13_sprint/N.py
@@ -1,23 +1,86 @@
-"""A. Генератор скобок."""
+"""N. Клумбы."""
 
 
-def read_input() -> tuple:
-    length = int(input())
-    return length
+def read_input():
+    gardeners = int(input())
+    flowerbeds = 0
+    garden = []
+    while flowerbeds < gardeners:
+        garden.append(list(map(int, list(input().strip().split()))))
+        flowerbeds += 1
+    return garden
 
 
-def generator(length, left=0, right=0, result='', ):
-    if length == left and length == right:
-        print(result)
-    else:
-        # на прямом ходе
-        if length > left:
-            generator(length, left + 1, right, result + '(')
-        # на обратном ходу правые скобки контролим количеством левых
-        if left > right:
-            generator(length, left, right + 1, result + ')')
+def order_flowerbeds(garden: list) -> list:
+
+    if len(garden) == 1:
+        return garden
+
+    left = order_flowerbeds(garden[0: len(garden)//2])
+    right = order_flowerbeds(garden[len(garden)//2: len(garden)])
+    flowerbeds = []
+
+    if len(left) == len(right):
+        merged = False
+        start = left[0][0]
+
+        # без пересечений
+        if left[0][1] < right[0][0]:
+            end = left[0][1]
+        # c пересечением
+        elif left[0][1] >= right[0][0] and left[0][1] >= right[0][1]:
+            end = left[0][1]
+            merged = True
+        else:
+            end = right[0][1]
+            merged = True
+        flowerbeds.append([start, end])
+
+        if not merged:
+            return left + right
+        return flowerbeds
+
+    if len(left) > len(right):
+        one_one = order_flowerbeds(garden=[left[0], right[0]])
+        if len(one_one) == 2:
+            two_one = order_flowerbeds(garden=[left[1], right[0]])
+            if len(two_one) == 2:
+                flowerbeds = garden
+            else:
+                flowerbeds = [left[0]] + two_one
+        else:
+            flowerbeds = one_one + [left[1]]
+
+    if len(left) < len(right):
+        one_one = order_flowerbeds(garden=[left[0], right[0]])
+        if len(one_one) == 2:
+            two_one = order_flowerbeds(garden=[left[0], right[1]])
+            if len(two_one) == 2:
+                flowerbeds = garden
+            else:
+                flowerbeds = two_one + [right[0]]
+        else:
+            flowerbeds = one_one + [right[1]]
+
+    if len(flowerbeds) % 2 == 0:
+        flowerbeds = order_flowerbeds(flowerbeds)
+
+    return flowerbeds
+
+
+def output(flowerbeds):
+    # for i in range(len(flowerbeds)-1, -1, -1):
+    for i in range(0, len(flowerbeds)):
+        print(flowerbeds[i][0], flowerbeds[i][1])
+
+
+# def sort_by_list_start(obj: list) -> int:
+#     return obj[0]
 
 
 if __name__ == '__main__':
-    length = read_input()
-    generator(length)
+    garden = read_input()
+    # garden = sorted(garden, key=sort_by_list_start)
+    garden = sorted(garden)
+    flowerbeds = order_flowerbeds(garden)
+    output(flowerbeds)
