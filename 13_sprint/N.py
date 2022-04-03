e37917a357ab00b3efe257a1356ff7bea48b7d72
Timur Gain <timur.gain@gmail.com>
2022-04-04 00:21:12 +0300

    N. Клумбы - passed.

--- 13_sprint/N.py
+++ 13_sprint/N.py
@@ -1,4 +1,18 @@
-"""N. Клумбы."""
+"""N. Клумбы.
+Для ландшафтных работ было нанято n садовников. Каждый из них обрабатывал
+какой-то отрезок на схеме. Процесс был организован не очень хорошо, иногда
+один и тот же отрезок или его часть могли быть обработаны сразу несколькими
+садовниками. Таким образом, отрезки, обрабатываемые двумя разными садовниками,
+сливаются в один. Непрерывный обработанный отрезок затем станет клумбой.
+Нужно определить границы будущих клумб.
+Рассмотрим примеры.
+Пример 1:
+Два одинаковых отрезка [7, 8] и [7, 8] сливаются в один, но потом их накрывает
+отрезок [6, 10]. Таким образом, имеем две клумбы с координатами [2,3] и [6,10].
+Пример 2
+Отрезки [2,3], [3, 4] и [3,4] сольются в один отрезок [2,4]. Отрезок [5,6] ни
+с кем не объединяется, добавляем его в ответ.
+"""
 
 
 def read_input():
@@ -69,18 +83,16 @@ def order_flowerbeds(garden: list) -> list:
 
 
 def output(flowerbeds):
-    # for i in range(len(flowerbeds)-1, -1, -1):
     for i in range(0, len(flowerbeds)):
         print(flowerbeds[i][0], flowerbeds[i][1])
 
 
-# def sort_by_list_start(obj: list) -> int:
-#     return obj[0]
+def sort_by_list_start(obj: list) -> int:
+    return obj[0]
 
 
 if __name__ == '__main__':
     garden = read_input()
-    # garden = sorted(garden, key=sort_by_list_start)
-    garden = sorted(garden)
+    garden = sorted(garden, key=sort_by_list_start)
     flowerbeds = order_flowerbeds(garden)
     output(flowerbeds)
