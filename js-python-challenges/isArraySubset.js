856da40ec83a757537fa51befbd3a9e6046d207f
Timur Gain <timur.gain@gmail.com>
2023-05-15 12:03:31 +0400

    is array subset

---
+++ js-python-challenges/isArraySubset.js
@@ -0,0 +1,23 @@
+/**
+ * Given two arrays, return true if a one is subset for another, and false otherwise
+ *
+ * @param {array} source
+ * @param {array} subset
+ * @return {boolean}
+ */
+
+function isArraySubset(source, subset) {
+  if (source.length < subset.length) return false
+  for (let i = 0; i < subset.length; i++) {
+    const index = source.indexOf(subset[i])
+    if (index === -1) return false
+    source.splice(index, 1)
+  }
+  return true
+}
+
+
+a = [2, 3, 5, 8, 14] 
+b = [5, 8, 2, 8]
+
+console.log(isArraySubset(a, b))
