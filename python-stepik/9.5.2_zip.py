d26f4fe62f9761ff38c4abca60311f69d01e9954
Timur Gain <timur.gain@gmail.com>
2023-05-19 15:15:10 +0400

    init: restaurant-orders ts

---
+++ python-stepik/9.5.2_zip.py
@@ -0,0 +1,12 @@
+from typing import Generator, Iterable
+
+def multiply_collections(a: Iterable[int], b: Iterable[int]) -> Generator:
+    """Multiplies items in two collections."""
+    return map(lambda x: x[0] * x[1], [*zip(a, b)])
+
+
+if __name__ == '__main__' :
+    a = map(int, input().split(' '))
+    b = map(int, input().split(' '))
+    res = multiply_collections(a, b)
+    print(next(res), next(res), next(res))
