634b6e1309f3492c94daabebc797f56ce7af5b2c
Timur Gain <timur.gain@gmail.com>
2023-06-15 00:13:02 +0400

    interfaces and other means of typing objects

--- typescript-codecademy/self-driving-car/index.ts
+++ typescript-codecademy/self-driving-car/index.ts
@@ -1,2 +1,65 @@
 import { getObstacleEvents } from './computer-vision';
 
+interface Events {
+  [obsticle: string]: boolean;
+}
+
+interface AutonomousCar {
+  isRunning: boolean;
+  respond: (events: Events) => void;
+}
+
+interface AutonomousCarProps {
+  isRunning: boolean;
+  steeringControl: Steering;
+}
+
+interface Control {
+  execute: (command: string) => void;
+}
+
+interface Steering extends Control {
+  turn: (direction:string) => void;
+}
+
+class Car implements AutonomousCar {
+  isRunning;
+  steeringControl;
+
+  constructor(props: AutonomousCarProps) {
+    this.isRunning = props.isRunning;
+    this.steeringControl = props.steeringControl;
+  }
+
+  respond(events: Events) {
+    if (!this.isRunning) {
+      return console.log('The car is off')
+    }
+
+    Object.keys(events).forEach((eventKey) => {
+      if (!events[eventKey]) return
+      if (eventKey === 'ObstacleLeft') {
+        this.steeringControl.turn('right')
+      }
+      if (eventKey === 'ObstacleRight') {
+        this.steeringControl.turn('left')
+      }
+    })
+  }
+}
+
+class SteeringControl implements Steering {
+  execute(command: string) {
+    console.log(`Executing: ${command}`)
+  }
+  turn(direction: string) {
+    this.execute(direction)
+  }
+}
+
+
+// main
+
+const steering = new SteeringControl()
+const autonomousCar = new Car({isRunning: true, steeringControl: steering});
+autonomousCar.respond(getObstacleEvents())
