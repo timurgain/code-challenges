7248a367ddd20b5ba08c5e30ed23f0ea17d09e84
Timur Gain <timur.gain@gmail.com>
2023-06-30 13:34:05 +0400

    init: redux game project

---
+++ redux-codecademy/game/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import { store } from './app/store.js';
+// Add import statement below
+
+
+ReactDOM.render(
+  // Implement Provider component with store below
+
+    <App />,
+  document.getElementById('root')
+);
