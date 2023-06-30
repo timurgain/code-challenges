94bcafd910354ba55af1f98998eed05fdfa9c766
Timur Gain <timur.gain@gmail.com>
2023-06-30 17:19:31 +0400

    feat: react-redux game project

--- redux-codecademy/game/src/index.js
+++ redux-codecademy/game/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
 import { store } from './app/store.js';
-// Add import statement below
+import { Provider } from 'react-redux';
 
 
 ReactDOM.render(
   // Implement Provider component with store below
-
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
